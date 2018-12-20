3ab96594aadeef4dabbee694607e4ffd9a7cfdbc
majosalazar <mariaj.salazarf@gmail.com>
2018-12-20 16:45:00 -0300

    muestra los nombres de los pokemones

--- data.js
+++ data.js
@@ -1,8 +1,15 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+const dataPokemon = POKEMON.pokemon;
+console.log(dataPokemon);
 
-window.example = example;
+const namesPokemon = (dataPokemon) => {
+  let names = [];
+  for (let i = 0; i < dataPokemon.length; i++) {
+    names.push(dataPokemon[i].name)
+  }
+  return names;
+}
+
+window.namesPokemon = namesPokemon;
