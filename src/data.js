78c9793daefb4a555dab9b3a6555c7931307b898
majosalazar <mariaj.salazarf@gmail.com>
2019-01-02 10:15:31 -0300

    imprime en pantalla todos los pokemones con sus respectivos numero, imagen, nombre y tipo

--- src/data.js
+++ src/data.js
@@ -3,22 +3,12 @@
 
 const dataPokemon = POKEMON.pokemon;
 console.log(dataPokemon);
-/*
-const namesPokemon = (dataPokemon) => {
-  let names = [];
-  for (let i = 0; i < dataPokemon.length; i++) {
-    names.push(dataPokemon[i].name)
-  }
-  return names;
-}
-*/
+
 //guarda el numero de los pokemons
 const numsPokemon = dataPokemon.map(({num}) => [`${num}`]);
 
 //guarda el nombre de los pokemones
 const namesPokemon = dataPokemon.map(({name}) => [`${name}`]);
-console.log(namesPokemon)
-window.namesPokemon = namesPokemon;
 
 //guarda la imagen de los pokemons
 const imgsPokemon = dataPokemon.map(({img}) => [`${img}`]);
@@ -27,7 +17,6 @@ const imgsPokemon = dataPokemon.map(({img}) => [`${img}`]);
 const typesPokemon = dataPokemon.map(({type}) => [`${type}`])
 
 
-window.imgsPokemon = imgsPokemon;
 
 //prueba filter
 const filterPokemonName = dataPokemon.filter(dataPokemon => (dataPokemon.name === 'Bulbasour'));
