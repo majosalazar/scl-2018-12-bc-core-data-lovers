49ae24e3e0cbff89080a506120f80b28060efed6
majosalazar <mariaj.salazarf@gmail.com>
2019-01-04 09:25:40 -0300

    pagina principal muestra los pokemones, pero faltan mostrar algunas caracteristicas bien

--- main.js
+++ main.js
@@ -17,17 +17,24 @@ document.getElementById('listPokemon').innerHTML += `
 //entrar a carta principal de un pokemon
 dataPokemon.map(({id, num, name, img, type, height, weight, weaknesses, prev_evolution, next_evolution, spawn_chance}) => 
 document.getElementById(`${id}`).onclick = () => {
+  console.log(typesPokemon);
   document.getElementById('main').innerHTML += `
   <div class="card w-100" style="width: 15rem;">
     <img class="card-img-top" src=${img}>
     <div class="card-body">
       <p>${num}</p>
       <p>${name}</p>
-      <p>${type}</p>
+      <button class="${type[0]}">${type[0]}</button>
+      <button class="${type[1]}">${type[1]}</button>
+      <button class="${type[2]}">${type[2]}</button>
+      <button class="${type[3]}">typesPokemon</button>
+      <p>${type[0]}</p>
       <p>${height}</p>
       <p>${weight}</p>
       <p>${weaknesses}</p>
       <p>${prev_evolution}</p>
+      <p>${next_evolution}</p>
+      <p>${spawn_chance}</p>
     </div>
   </div>
   `
@@ -35,10 +42,11 @@ document.getElementById(`${id}`).onclick = () => {
 
 
 
-
+/*
 dataPokemon.forEach(function(element){
   console.log(element);
 })
+*/
 
 
 
