eac4ec19495c6556d20765e5a9d1eaade6cc4dbe
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-01-05 15:25:55 -0300

    Mejorando Filtro por Tipo

--- data.js
+++ data.js
@@ -1,5 +1,4 @@
 const dataPokemon = POKEMON.pokemon;
-console.log(dataPokemon);
 
 //guarda el numero de los pokemons
 const numsPokemon = dataPokemon.map(({num}) => [`${num}`]);
@@ -12,7 +11,6 @@ const imgsPokemon = dataPokemon.map(({img}) => [`${img}`]);
 
 //guarda el tipo de los pokemones
 const typesPokemon = dataPokemon.map(({type}) => [`${type}`])
-const typesOnePokemon = typesPokemon.map(({}))
 
 //guarda el id de los pokemones
 const idPokemon = dataPokemon.map(({id}) => [`${id}`])
@@ -23,77 +21,21 @@ const filterPokemonName = dataPokemon.filter(dataPokemon => (dataPokemon.name ==
 
 //Clasifico por tipo
 
-let i=0;
-let grass = [];
-const typeGrass = dataPokemon.filter((types) => types.type[i] === "Grass")
-const prueba = typeGrass.forEach((element) => {
-  grass.push(element.name, element.img)
-});
+//Filtrar tipos fuego
+const typeFire = dataPokemon.filter((types) => {return types.type.indexOf ("Fire") >=0});
 
-//.map((types => types.img + types.name));
-console.log(grass);
+//Filtrar tipos Hielo
+const typeIce = dataPokemon.filter((types) =>{return types.type.indexOf("Ice") >=0});
 
-const ent = typeGrass.forEach((element) => {
-  console.log(Object.entries(element))
-});
+//Filtrar tipos Flying
+const typeFlying = dataPokemon.filter((types) => {return types.type.indexOf("Flying") >=0});
 
+//Filter Psychic
+const typePsychic = dataPokemon.filter((types) => {return types.type.indexOf("Psychic") >=0});
 
-//.map((types)=>{return types.name;}); 
-//.map((types)=>{return types.name && types.img;}); 
-//window.typeGrass = typeGrass;
+//Filter Grass
+const typeGrass = dataPokemon.filter((types) => {return types.type.indexOf("Grass") >=0});
 
-
-
-const typePoison = dataPokemon.filter((types) => types.type[i] === "Poison")
-.map((types)=>{return types.img;});
-
-const typeFire = dataPokemon.filter((types) => types.type[i]==="Fire")
-.map((types)=>{return types.img;});
-
-const typeIce = dataPokemon.filter((types) => types.type[i]==="Ice")
-.map((types)=>{return types.img;});
-
-const typeFlyin = dataPokemon.filter((types) => types.type[i]==="Flying")
-.map((types)=>{return types.img;});
-
-const typePsychic = dataPokemon.filter((types) => types.type[i]==="Psychic")
-.map((types)=>{return types.img;});
-
-const typeWater= dataPokemon.filter((types) => types.type[i]==="Water")
-.map((types)=>{return types.img;});
-
-const typeGround = dataPokemon.filter((types) => types.type[i]==="Ground")
-.map((types)=>{return types.img;});
-
-const typeRock = dataPokemon.filter((types) => types.type[i]==="Rock")
-.map((types)=>{return types.img;});
-
-const typeElectric= dataPokemon.filter((types) => types.type[i]==="Electric")
-.map((types)=>{return types.img;});
-
-const typeBug = dataPokemon.filter((types) => types.type[i]==="Bug")
-.map((types)=>{return types.img;});
-
-const typeNormal = dataPokemon.filter((types) => types.type[i]==="Normal")
-.map((types)=>{return types.img;});
-
-const typeFighting = dataPokemon.filter((types) => types.type[i]==="Fighting")
-.map((types)=>{return types.img;});
-
-const typeFairy = dataPokemon.filter((types) => types.type[i]==="Fairy")
-.map((types)=>{return types.img;});
-
-const typeGhost= dataPokemon.filter((types) => types.type[i]==="Ghost")
-.map((types)=>{return types.img;});
-
-const typeDark = dataPokemon.filter((types) => types.type[i]==="Dark")
-.map((types)=>{return types.img;});
-
-const typeSteel = dataPokemon.filter((types) => types.type[i]==="Steel")
-.map((types)=>{return types.img;});
-
-const typeDragon= dataPokemon.filter((types) => types.type[i]==="Dragon")
-.map((types)=>{return types.img;});
 
 window.typesPokemon;
 
