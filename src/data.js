6411963b09a1074366b39ba069ad27601c132c63
MarthaLizarazo <johanalizarazo_8@hotmail.com>
2019-01-09 13:07:57 -0300

    Prueba Grafico

--- src/data.js
+++ src/data.js
@@ -90,7 +90,44 @@ window.data = {
   },
 
   computeStats: (dataPokemon) => {
+    
 
   }
 }
 */
+// Graficos
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+var data = google.visualization.arrayToDataTable([
+  ['Task', 'Hours per Day'],
+  ['Fuego',     counterFire],
+  ['Hielo',      counterIce],
+  ['Volador',  counterFlying],
+  ['Psíquico', counterPsychic],
+  ['Hierba ',    counterGrass],
+  ['Veneno ',    counterPoison],
+  ['Hierba ',    counterGrass],
+  ['Agua ',    counterWater],
+  ['Tierra ',    counterGround],
+  ['Roca ',    counterRock],
+  ['Eléctrico ',    counterElectric],
+  ['Insecto ',    counterBug],
+  ['Normal ',    counterNormal],
+  ['Lucha ',    counterFighting],
+  ['Hada ',    counterFire],
+  ['Oscuridad ',    counterDark],
+  ['Fantasma ',    counterGhost],
+  ['Acero ',    counterSteel],
+  ['Dragon ',    counterDragon]
+  
+]);
+
+var options = {
+  title: 'My Daily Activities',
+  pieHole: 0.4,
+};
+
+var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+chart.draw(data, options);
+}
